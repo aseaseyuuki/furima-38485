d7d05efa13652c762b0eeac36ea746e108821035
aseaseyuuki <114049905+aseaseyuuki@users.noreply.github.com>
2022-10-25 16:33:09 +0900

    heroku Basic認証の修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,7 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :family_name, :family_name_kana, :first_name, :first_name_kana, :birth_day])
   end
 
+  
   def basic_auth
     authenticate_or_request_with_http_basic do |nickname, password|
     nickname == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
