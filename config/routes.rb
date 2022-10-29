e9f5db262777df18beab7f2690b604cbd69ca1cc
aseaseyuuki <114049905+aseaseyuuki@users.noreply.github.com>
2022-10-29 15:07:31 +0900

    商品詳細表示機能 記述訂正

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'items#index'
   resources :users, only: [:index, :new, :create, :edit, :update, :destroy]
-  resources :items, only: [:new, :create, :show]
+  resources :items, only: [:new, :create, :index, :show]
 end
