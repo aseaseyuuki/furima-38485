2b97e46347b97459005aadf2a1816134cd00aacc
aseaseyuuki <114049905+aseaseyuuki@users.noreply.github.com>
2022-11-03 19:06:01 +0900

    テストコード

--- spec/factories/order_destinations.rb
+++ spec/factories/order_destinations.rb
@@ -1,9 +1,9 @@
 FactoryBot.define do
   factory :order_destination do
-    posta_code { '123-4567' }
+    post_code { '123-4567' }
     shipping_origin_id { 2 }
     city { '東京都' }
-    house_number { '1-1' }
+    address { '1-1' }
     building_name { '東京ハイツ' }
     phone_number { "09012345678" }
   end
--- spec/models/order_destination_spec.rb
+++ spec/models/order_destination_spec.rb
@@ -2,18 +2,17 @@ require 'rails_helper'
 
 RSpec.describe OrderDestination, type: :model do
   describe '購入情報の保存' do
-  before do
-    @user = FactoryBot.create(:user)
-    @item = FactoryBot.build(:item)
-    @item.save
-    @order_destination = FactoryBot.build(:order_destination, user_id: @user.id, item_id: @item.id)
-   end
+    before do
+      user = FactoryBot.create(:user)
+      item = FactoryBot.create(:item)
+      @order_destination = FactoryBot.build(:order_destination, user_id: user.id, item_id: item.id)
+    end
 
     context '内容に問題ない場合' do
       it 'すべての値が正しく入力されていれば保存できること' do
         expect(@order_destination).to be_valid
       end
-     end
+    
       it 'building_nameは空でも保存できること' do
         @order_destination.building_name = ''
         expect(@order_destination).to be_valid
@@ -22,7 +21,7 @@ RSpec.describe OrderDestination, type: :model do
 
     context '内容に問題がある場合' do
       it 'post_codeが空だと保存できないこと' do
-        @order_destination.post_code = nil
+        @order_destination.post_code = ''
         @order_destination.valid?
         expect(@order_destination.errors.full_messages).to include "Post code can't be blank"
       end
@@ -31,46 +30,46 @@ RSpec.describe OrderDestination, type: :model do
         @order_destination.valid?
         expect(@order_destination.errors.full_messages).to include "Post code is invalid. Include hyphen(-)"
       end
-      it 'shipping_originが未選択では登録できない' do
-        @order_destination.shipping_origin_id = nil
+      it 'shipping_originが--では登録できない' do
+        @order_destination.shipping_origin_id = 1
         @order_destination.valid?
-        expect(@order_destination.errors.full_messages).to include "Shipping origin can't be blank"
+        expect( @order_destination.errors.full_messages).to include 
       end
       it 'cityが空だと保存できないこと' do
-        @order_destination.city = nil
+        @order_destination.city = ''
         @order_destination.valid?
         expect(@order_destination.errors.full_messages).to include "City can't be blank"
       end
       it 'addressが空だと保存できないこと' do
-        @order_destination.address = nil
+        @order_destination.address = ''
         @order_destination.valid?
         expect(@order_destination.errors.full_messages).to include "Address can't be blank"
       end
       it 'phone_numberが空だと保存できないこと' do
-        order_destination.phone_number = nil
+        @order_destination.phone_number = ''
         @order_destination.valid?
         expect(@order_destination.errors.full_messages).to include "Phone number can't be blank"
       end
       it 'phone_numberが半角数値で10桁以下だと保存できないこと' do
         @order_destination.phone_number = '123456789'
         @order_destination.valid?
-        expect(@order_destination.errors.full_messages).to include "Phone number is too short (minimum is 9 characters)"
+        expect(@order_destination.errors.full_messages).to include "Phone number is invalid"
       end
       it 'phone_numberが半角数値で11桁以上だと保存できないこと' do
         @order_destination.phone_number = '012345678900'
         @order_destination.valid?
-        expect(@order_destination.errors.full_messages).to include "Phone number is too long (minimum is 12 characters)"
+        expect(@order_destination.errors.full_messages).to include "Phone number is too long (maximum is 11 characters)"
       end
-      it 'order_destinationが空だと保存できないこと' do
-        @order_destination. = nil
+      it 'userが空だと保存できないこと' do
+        @order_destination.user_id = ''
         @order_destination.valid?
-        expect(@order_destination.errors.full_messages).to include "Phone number can't be blank"
+        expect(@order_destination.errors.full_messages).to include "User can't be blank"
+      end
       it 'itemが紐付いていないと保存できないこと' do
-         @order_destination.item_id = nil
+         @order_destination.item_id = ''
          @order_destination.valid?
          expect( @order_destination.errors.full_messages).to include "Item can't be blank"
-        end
       end
     end
   end
-
+end
