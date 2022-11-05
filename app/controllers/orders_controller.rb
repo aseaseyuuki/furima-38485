b955b95a5fd571891e68bce40a901585c6c3e84f
aseaseyuuki <114049905+aseaseyuuki@users.noreply.github.com>
2022-11-05 20:49:25 +0900

    テストコード追記 URL直入力不可

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,9 +1,14 @@
 class OrdersController < ApplicationController
   before_action :authenticate_user!, only: :index
+  before_action :set_item, only: [:index, :correct_post]
+  before_action :correct_post, only: [:index]
+ 
 
   def index
     @order_destination = OrderDestination.new
-    @item = Item.find(params[:item_id])
+    if current_user == @item.user
+       redirect_to root_path
+    end
   end
 
 
@@ -14,7 +19,7 @@ class OrdersController < ApplicationController
      pay_item
      @order_destination.save
      redirect_to root_path
-    else
+    else 
       render 'index'
     end  
   end
@@ -23,6 +28,16 @@ class OrdersController < ApplicationController
 
   def order_params
     params.require(:order_destination).permit(:post_code, :shipping_origin_id, :city, :address, :building_name, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
+  end
+
+  def set_item
+    @item = Item.find(params[:item_id])
+   end
+
+  def correct_post
+    unless @item.order.nil? 
+     redirect_to root_path
+    end 
   end
 
   def pay_item
--- spec/models/order_destination_spec.rb
+++ spec/models/order_destination_spec.rb
@@ -3,22 +3,19 @@ require 'rails_helper'
 RSpec.describe OrderDestination, type: :model do
   describe '購入情報の保存' do
     before do
-      order = FactoryBot.create(:order)
       user = FactoryBot.create(:user)
       item = FactoryBot.create(:item)
-      @order_destination = FactoryBot.build(:order_destination, user_id: user.id, item_id: item.id, order_id: order.id)
+      @order_destination = FactoryBot.build(:order_destination, user_id: user.id, item_id: item.id)
     end
 
     context '内容に問題ない場合' do
       it 'すべての値が正しく入力されていれば保存できること' do
         expect(@order_destination).to be_valid
       end
+
       it 'building_nameは空でも保存できること' do
         @order_destination.building_name = ''
         expect(@order_destination).to be_valid
-      end
-      it "priceとtokenがあれば保存ができること" do
-        expect( @order_destinationr).to be_valid
       end
     end
 
@@ -71,18 +68,18 @@ RSpec.describe OrderDestination, type: :model do
       it 'itemが紐付いていないと保存できないこと' do
          @order_destination.item_id = ''
          @order_destination.valid?
-         expect( @order_destination.errors.full_messages).to include "Item can't be blank"
-      end
-      it "priceが空では保存ができないこと" do
-        @order_destination.price = nil
-        @order_destination.valid?
-        expect(@order_destination.errors.full_messages).to include "Price can't be blank"
+         expect(@order_destination.errors.full_messages).to include "Item can't be blank"
       end
       it "tokenが空では登録できないこと" do
-        @order_destinationr.token = nil
-        @order_destinationr.valid?
-        expect( @order_destinationr.errors.full_messages).to include "Token can't be blank"
+        @order_destination.token = nil
+        @order_destination.valid?
+        expect(@order_destination.errors.full_messages).to include "Token can't be blank"
       end
+      # it "priceが空では保存ができないこと" do
+        # @order_destination.price =''
+        # @order_destination.valid?
+        # @expect(@order_address.errors.full_messages).to include("Price can't be blank")
+      # end
     end
   end
 end
