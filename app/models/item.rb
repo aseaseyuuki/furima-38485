8eb0374e0c86094d1043e0071cb09ec09e01540f
aseaseyuuki <114049905+aseaseyuuki@users.noreply.github.com>
2022-11-07 14:56:54 +0900

    レビュー後修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -11,25 +11,23 @@ class Item < ApplicationRecord
   
 
 
-  validates :name, presence: true
-  with_options presence: true, format: { with: /\A[0-9]+\z/ } do
-  validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 },
-                      presence: { message: "can't be blank" }
+  
+  with_options presence: true do
+  validates :name
+  validates :description               
+  validates :image
   end
-  validates :description,            presence: true
-  validates :status_id,              presence: true
-  validates :delivery_charge_id,     presence: true
-  validates :shipping_origin_id,     presence: true
-  validates :shipping_day_id,        presence: true
-  validates :category_id,            presence: true
-  validates :image,                  presence: true
 
-  with_options numericality: { other_than: 1 } do
+  with_options presence: true, format: { with: /\A[0-9]+\z/, message: 'Half-width number' } do
+  validates :price,
+            numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, message: 'Out of setting range' }
+  end 
+
+  with_options presence: true, numericality: { other_than: 1 } do
   validates :status_id
   validates :delivery_charge_id
   validates :shipping_origin_id
   validates :shipping_day_id
   validates :category_id
-end
-validates :price, numericality: true
+  end
 end
